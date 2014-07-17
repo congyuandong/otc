583422633362073bc3d15862c85d8c98d8b2bfa0
congyuandong <congyuandong@gmail.com>
2014-07-18 00:34:23 +0800

    修改中小股转

--- otc/views.py
+++ otc/views.py
@@ -8,9 +8,9 @@ import simplejson as json
 from otc.models import industry,otc_new,otc_hot,otc_study,otc_base
 
 def index(request):
-	otc_new_list_sanban = otc_new.objects.filter(new_region__reg_name__exact='新三板')[:7]
+	otc_new_list_sanban = otc_new.objects.filter(new_region__reg_name__exact='中小股转')[:7]
 	otc_new_list_shanghai = otc_new.objects.filter(new_region__reg_name__exact='上海')[:7]
-	otc_new_list_other = otc_new.objects.exclude(new_region__reg_name__exact='上海').exclude(new_region__reg_name__exact='新三板')[:7]
+	otc_new_list_other = otc_new.objects.exclude(new_region__reg_name__exact='上海').exclude(new_region__reg_name__exact='中小股转')[:7]
 	otc_hot_list = otc_hot.objects.order_by('-hot_sum_trans')[:10]
 	otc_study_list = otc_study.objects.order_by('stu_date')[:8]
 	otc_base_last = otc_base.objects.order_by('base_date')[:1]

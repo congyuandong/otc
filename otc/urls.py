831af18bc07bf96470107a506048e27c2b5af033
congyuandong <congyuandong@gmail.com>
2014-10-16 19:02:46 +0800

    增加市场指数图表

--- otc/urls.py
+++ otc/urls.py
@@ -6,4 +6,5 @@ urlpatterns = patterns('',
 	url(r'^industry_pie/$', views.industry_pie, name='industry_pie'),
 	url(r'^industry_column/$', views.industry_column, name='industry_column'),
 	url(r'^industry_line_chart/$', views.industry_line_chart, name='industry_line_chart'),
+	url(r'^otc_general_line_chart/$', views.otc_general_line_chart, name='otc_general_line_chart'),
 	)
